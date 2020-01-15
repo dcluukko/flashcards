8d2a5e4806762602dfa699d954a82b19fd4518e5
dl <dluukko@iyrs.edu>
2020-01-14 20:03:36 -0800

    created a second, independent button that could toggle a definitiion

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document). ready(function() {
 	$("button#word1").click(function() {
-		$(".vocab_word").toggle();		
-		$(".definition").toggle(); 
+		$(".vocab_word1").toggle();		
+		$(".definition1").toggle(); 
 	});
-});
+
+
+		$("button#word2").click(function() {
+			$(".vocab_word2").toggle();		
+			$(".definition2").toggle(); 
+		});
+	});
+
+	
